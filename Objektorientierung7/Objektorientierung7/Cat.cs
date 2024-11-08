abef196924706b731e80077daf28d2d02421a7fc
Lars <lars.veljaca1@icloud.com>
2024-11-08 11:52:42 +0100

    Cat_name_and_age

---
+++ Objektorientierung7/Objektorientierung7/Cat.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Objektorientierung7
+{
+    public class Cat
+    {
+        private string? _Color = null;
+        private DateTime _BirthDate;
+
+        public string? Color
+        {
+            get { return _Color; }
+            set
+            {
+                if (_Color == value) return;
+                _Color = value;
+            }
+        }
+        public int Age
+        {
+            get
+            {
+                var today = DateTime.Today;
+                int age = today.Year - _BirthDate.Year;
+                if (_BirthDate > today.AddYears(-age)) age--;
+                return age;
+            }
+        }
+        public Cat(DateTime birthdate)
+        {
+            _BirthDate = birthdate;
+        }
+    }
+}
