85f753a0c7bdfae9cd898b16d0275e2cceea9d73
Lars <lars.veljaca1@icloud.com>
2024-11-08 14:31:33 +0100

    Diesmal wirklich fertig und Funktional!

--- Objektorientierung7/Objektorientierung7/Program.cs
+++ Objektorientierung7/Objektorientierung7/Program.cs
@@ -2,7 +2,7 @@
 using Objektorientierung7;
 
 //Console.WriteLine("Hello World!");
-//Cat myCat = new Cat(new DateTime(2008, 01, 18)); // erstellt eine Instanz der Klasse Cat und weißt sie der Variable mit dem Namen myCat zu
+Cat myCat = new Cat(new DateTime(2008, 01, 18)); // erstellt eine Instanz der Klasse Cat und weißt sie der Variable mit dem Namen myCat zu
 //myCat.Color = "black";
 
 //Console.WriteLine("The color of my Cat is " + myCat.Color);
